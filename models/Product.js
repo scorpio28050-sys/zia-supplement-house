278debfb7d5c885034b4c1ef568acfbf0eda7dad
scorpio28050-sys <scorpio28050@gmail.com>
2026-02-10 19:57:17 +0200

    Create Product.js model file with mongoose schema for products

---
+++ models/Product.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    category: { type: String, required: true },
+    price: { type: Number, required: true },
+    discount: { type: Number, default: 0 },
+    images: [{ type: String }]
+}, { timestamps: true });
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
